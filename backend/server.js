111ad11614a03eb0295a065fbc29e2c7450c02c7
Polina <polinaostanina2000@gmail.com>
2026-06-09 14:43:52 +0500

    update dashboard and workflow

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,6 @@ app.get("/", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Сервер запущен на порту ${PORT}`);
 });
